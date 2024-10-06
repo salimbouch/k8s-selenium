54c168399f6b6e1bf41b9bb28545c10317eefa50
Salim <salim.bouchara@capgemini.com>
2024-10-06 05:43:48 +0200

    fixed tests

--- src/test/java/com/example/demo/DemoApplicationTest.java
+++ src/test/java/com/example/demo/DemoApplicationTest.java
@@ -55,7 +55,7 @@ public class DemoApplicationTest {
         WebElement button2 = driver.findElement(By.xpath("//button[text()='Button 2']"));
         button2.click();
         WebElement output = driver.findElement(By.id("output"));
-        Assert.assertEquals(output.getText(), "Button ? was pressed");
+        Assert.assertEquals(output.getText(), "Button 2 was pressed");
     }
 
     @Test
