d018495f02f2651a0dc3cbf249cb006de445a56a
salimbouch <145128725+salimbouch@users.noreply.github.com>
2024-10-07 15:55:46 +0200

    changed button color

--- src/test/java/com/example/demo/DemoApplicationTest.java
+++ src/test/java/com/example/demo/DemoApplicationTest.java
@@ -18,7 +18,6 @@ public class DemoApplicationTest {
 
     @BeforeMethod
     public void setup() throws Exception {
-
         String hubUrl = System.getProperty("HUB_URL", "http://selenium-hub:4444/wd/hub");
         baseUrl = System.getProperty("APP_URL", "http://web-app:8080");
         String browser = System.getenv("BROWSER");
